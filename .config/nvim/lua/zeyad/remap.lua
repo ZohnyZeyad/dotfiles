3e64cfaa6dceef487f6832a3e9cc16d92e1adc75
Zohny, Zeyad [PS] <zeyad.zohny@cengage.com>
2024-10-29 16:47:09 +0100

    Fixed Toggle Comment remap.

--- .config/nvim/lua/zeyad/remap.lua
+++ .config/nvim/lua/zeyad/remap.lua
@@ -26,7 +26,8 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<C-z>', '<cmd>redo<CR>')
 
-vim.keymap.set({"n", "v"}, "<C-_>", "gcc", { remap = true, desc = 'Toggle comment line'})
+vim.keymap.set("n", "<C-_>", "gcc", { remap = true, desc = 'Toggle comment line'})
+vim.keymap.set("v", "<C-_>", "gc", { remap = true, desc = 'Toggle comment'})
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
