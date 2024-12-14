1987b883166e8ae0388938c9699937ae61cb08cf
ZohnyZeyad <ht1y0v@mailbox.unideb.hu>
2024-12-15 00:18:46 +0100

    Cleaned up remaps.

--- .config/nvim/lua/zeyad/remap.lua
+++ .config/nvim/lua/zeyad/remap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pv", '<cmd>Oil<CR>')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -16,7 +15,6 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("v", "p", '"_dP', { desc = 'Paste without overwriting register' })
@@ -25,7 +23,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "==", "gg<S-v>G", { desc = 'Select All' })
 
--- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<C-z>', '<cmd>redo<CR>')
@@ -43,18 +40,6 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-local set = vim.opt_local
-vim.api.nvim_create_autocmd("TermOpen", {
-  group = vim.api.nvim_create_augroup("custom_term_open", {}),
-  callback = function()
-    set.number = false
-    set.relativenumber = false
-    set.scrolloff = 5
-
-    vim.bo.filetype = "terminal"
-  end,
-})
-
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('t', '<C-w>h', "<C-\\><C-n><C-w>h", { silent = true })
 vim.keymap.set('t', '<C-w>j', "<C-\\><C-n><C-w>j", { silent = true })
@@ -68,6 +53,18 @@ vim.keymap.set('n', '<A-h>', '<C-w>h', { silent = true })
 vim.keymap.set('n', '<A-l>', '<C-w>l', { silent = true })
 vim.keymap.set('n', '<A-j>', '<C-w>j', { silent = true })
 vim.keymap.set('n', '<A-k>', '<C-w>k', { silent = true })
+
+local set = vim.opt_local
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom_term_open", {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 8
+
+    vim.bo.filetype = "terminal"
+  end,
+})
 
 vim.keymap.set("n", "<leader>st", function()
   vim.cmd.new()
@@ -90,11 +87,6 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Execute the current line" })
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
 vim.keymap.set("n", "<leader>sr", "<cmd>SudaRead<CR>", { desc = "Re-open the current filw with sudo" })
-
---[[ vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end) ]] --
