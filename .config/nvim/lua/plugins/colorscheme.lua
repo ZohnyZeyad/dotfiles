d098c4243267a3a599505e5f38fc1cc14646023d
ZohnyZeyad <ht1y0v@mailbox.unideb.hu>
2024-12-15 00:06:45 +0100

    Fixed theme transparency.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,54 @@
 function ColorMyPencils(color)
   color = color or "rose-pine"
   vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
 
-  -- {
-  --     "Mofiqul/dracula.nvim",
-  -- },
+  {
+    "Mofiqul/dracula.nvim",
+  },
+  {
+    "erikbackman/brightburn.vim",
+  },
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {},
+    config = function()
+      ColorMyPencils()
+    end
+  },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "moon",         -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true,     -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = "dark", -- style for sidebars, see below
+          floats = "dark",   -- style for floating windows
+        },
+      })
+    end
+  },
   {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
       require('rose-pine').setup({
+        dark_variant = "main", -- main, moon, or dawn
+        dim_inactive_windows = false,
         disable_background = true,
 
         enable = {
@@ -21,10 +57,11 @@ return {
 
         styles = {
           bold = true,
-          italic = true,
-          transparency = false,
-        },
+          italic = false,
+          transparency = true,
+        }
       })
+
       ColorMyPencils()
     end
   },
