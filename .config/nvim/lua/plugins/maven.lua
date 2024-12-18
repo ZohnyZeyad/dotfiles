f51a7c33b280ce138d5879a697048619f709dce5
ZohnyZeyad <zeyadalaaeldin20@gmail.com>
2024-12-18 18:29:33 +0100

    Added global .scalafmt.conf

--- .config/nvim/lua/plugins/maven.lua
+++ .config/nvim/lua/plugins/maven.lua
@@ -4,7 +4,7 @@ return {
   dependencies = "nvim-lua/plenary.nvim",
   config = function()
     require('maven').setup({
-      executable = "./mvnw",
+      executable = "mvn",
       commands = {
         { cmd = { "clean", "compile" }, desc = "clean then compile" },
         { cmd = { "clean", "package" }, desc = "clean then package" },
