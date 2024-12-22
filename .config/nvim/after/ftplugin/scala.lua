8a495cc099d7d4d84a1450fd98538d48ce0f0813
ZohnyZeyad <ht1y0v@mailbox.unideb.hu>
2024-12-22 13:59:31 +0100

    Updated scala formatting.

--- .config/nvim/after/ftplugin/scala.lua
+++ .config/nvim/after/ftplugin/scala.lua
@@ -1,2 +1,15 @@
+vim.bo.formatprg = "scalafmt --stdin"
+
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = {
+--     "*.scala",
+--     "*.sc",
+--   },
+--   callback = function()
+--     vim.cmd("silent! !scalafmt --stdin < % > %")
+--   end
+-- })
+
 vim.opt.shiftwidth = 2
+vim.opt_local.tabstop = 2
 vim.opt.expandtab = true
