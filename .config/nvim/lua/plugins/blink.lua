3118be30457a4cf5e675b02c57d74195746f9e44
ZohnyZeyad <zeyadalaaeldin20@gmail.com>
2025-02-26 17:01:29 +0100

    Refactored blink.lua.

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,4 +1,138 @@
+-- Snippets expansion function
+local function snippets_expand(snippet)
+  require('luasnip.loaders.from_vscode').lazy_load()
+
+  local luasnip = require('luasnip')
+  luasnip.filetype_extend('java', { 'javadoc', 'java-tests' })
+  luasnip.filetype_extend('scala', { 'javadoc' })
+  luasnip.filetype_extend('sh', { 'shelldoc' })
+  luasnip.filetype_extend('typescript', { 'tsdoc' })
+  luasnip.filetype_extend('javascript', { 'jsdoc' })
+
+  luasnip.lsp_expand(snippet)
+end
+
+-- Snippets active check function
+local function snippets_active(filter)
+  local luasnip = require('luasnip')
+  if filter and filter.direction then
+    return luasnip.jumpable(filter.direction)
+  end
+  return luasnip.in_snippet()
+end
+
+-- Snippets jump function
+local function snippets_jump(direction)
+  require('luasnip').jump(direction)
+end
+
+-- Default completion sources function
+local function sources_default_function(_)
+  local ok, node = pcall(vim.treesitter.get_node)
+  local defaults = {
+    'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'ripgrep',
+  }
+  if ok and node and vim.tbl_contains({
+        'comment',
+        'comment_content',
+        'line_comment',
+        'block_comment',
+        "string",
+        "string_content",
+      }, node:type()) then
+    -- In comment or string, only suggest path and buffer completions
+    return { 'path', 'buffer' }
+  else
+    return defaults -- Otherwise, use default sources
+  end
+end
+
+-- Cmdline completion sources function
+local function cmdline_sources_function()
+  local _, type = pcall(vim.fn.getcmdtype)
+  if type == '/' or type == '?' then
+    return { 'buffer', 'path' } -- For search commands, use buffer and path
+  end
+  if type == ':' then
+    return { 'cmdline' } -- For command-line commands, use cmdline source
+  end
+  return {}              -- Otherwise, no sources
+end
+
+-- Define completion providers for sources
+local completion_providers = {
+  lsp = {
+    name = "lsp",
+    enabled = true,
+    module = "blink.cmp.sources.lsp",
+    async = true,
+    score_offset = 90,
+    fallbacks = { 'snippets', 'ctags', 'buffer' }
+  },
+
+  lazydev = {
+    name = 'LazyDev',
+    module = 'lazydev.integrations.blink',
+    score_offset = 100,
+    fallbacks = { 'lsp' },
+  },
+
+  avante = {
+    module = 'blink-cmp-avante',
+    name = 'Avante',
+    score_offset = 150,
+    opts = {},
+  },
+
+  path = {
+    name = "Path",
+    module = "blink.cmp.sources.path",
+    score_offset = 3,
+    fallbacks = { "snippets", "buffer", "ripgrep" },
+    opts = {
+      trailing_slash = false,
+      label_trailing_slash = true,
+      get_cwd = function(context) -- Function to get current working directory for path completion
+        return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+      end,
+      show_hidden_files_by_default = true,
+    },
+  },
+
+  buffer = {
+    name = "Buffer",
+    enabled = true,
+    max_items = 3,
+    module = "blink.cmp.sources.buffer",
+    min_keyword_length = 3,
+  },
+
+  snippets = {
+    name = "snippets",
+    enabled = true,
+    max_items = 3,
+    module = "blink.cmp.sources.snippets",
+    min_keyword_length = 4,
+    score_offset = 85,
+  },
+
+  ripgrep = {
+    module = 'blink-ripgrep',
+    name = 'Ripgrep',
+    score_offset = -100,
+    opts = {
+      prefix_min_len = 3,
+      context_size = 5,
+      max_filesize = '1M',
+      additional_rg_options = {},
+      search_casing = '--smart-case',
+      fallback_to_regex_highlighting = true,
+    },
+  },
+}
+
 return {
+  -- Compatibility module for blink.nvim
   {
     'saghen/blink.compat',
     version = '*',
@@ -7,6 +141,7 @@ return {
     opts = {},
   },
 
+  -- Main blink.cmp configuration
   {
     'saghen/blink.cmp',
     version = 'v0.*',
@@ -21,6 +156,7 @@ return {
       'rafamadriz/friendly-snippets',
       'L3MON4D3/LuaSnip',
       'mikavilpas/blink-ripgrep.nvim',
+      'Kaiser-Yang/blink-cmp-avante',
     },
 
     opts = {
@@ -35,26 +171,9 @@ return {
 
       snippets = {
         preset = 'luasnip',
-        expand = function(snippet)
-          require('luasnip.loaders.from_vscode').lazy_load()
-
-          local luasnip = require('luasnip')
-          luasnip.filetype_extend('java', { 'javadoc', 'java-tests' })
-          luasnip.filetype_extend('scala', { 'javadoc' })
-          luasnip.filetype_extend('sh', { 'shelldoc' })
-          luasnip.filetype_extend('typescript', { 'tsdoc' })
-          luasnip.filetype_extend('javascript', { 'jsdoc' })
-
-          luasnip.lsp_expand(snippet)
-        end,
-        active = function(filter)
-          local luasnip = require('luasnip')
-          if filter and filter.direction then
-            return luasnip.jumpable(filter.direction)
-          end
-          return luasnip.in_snippet()
-        end,
-        jump = function(direction) require('luasnip').jump(direction) end,
+        expand = snippets_expand, -- Function to expand snippets
+        active = snippets_active, -- Function to check if snippets are active
+        jump = snippets_jump,     -- Function to jump between snippet locations
       },
 
       sources = {
@@ -62,115 +181,15 @@ return {
           return vim.bo.filetype == 'markdown' and 2 or 0
         end,
 
-        default = function(_)
-          local ok, node = pcall(vim.treesitter.get_node)
-          local defaults = {
-            'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'ripgrep',
-          }
-          if ok and node and vim.tbl_contains({
-                'comment',
-                'comment_content',
-                'line_comment',
-                'block_comment',
-                "string",
-                "string_content",
-              }, node:type()) then
-            return { 'path', 'buffer' }
-          else
-            return defaults
-          end
-        end,
+        default = sources_default_function,
 
         per_filetype = {
           lua = { 'lazydev', 'lsp', 'snippets', 'buffer', 'path' },
-          AvanteInput = { 'avante_commands', 'avante_mentions', 'avante_files' },
+          AvanteInput = { 'avante' },
           codecompanion = { "codecompanion" },
         },
 
-        providers = {
-          lsp = {
-            name = "lsp",
-            enabled = true,
-            module = "blink.cmp.sources.lsp",
-            async = true,
-            score_offset = 90,
-            fallbacks = { 'snippets', 'ctags', 'buffer' }
-          },
-
-          lazydev = {
-            name = 'LazyDev',
-            module = 'lazydev.integrations.blink',
-            score_offset = 100,
-            fallbacks = { 'lsp' },
-          },
-
-          avante_commands = {
-            name = "avante_commands",
-            module = "blink.compat.source",
-            score_offset = 110, -- show at a higher priority than lsp
-            opts = {},
-          },
-
-          avante_files = {
-            name = "avante_files",
-            module = "blink.compat.source",
-            score_offset = 120, -- show at a higher priority than lsp
-            opts = {},
-          },
-
-          avante_mentions = {
-            name = "avante_mentions",
-            module = "blink.compat.source",
-            score_offset = 1000, -- show at a higher priority than lsp
-            opts = {},
-          },
-
-          path = {
-            name = "Path",
-            module = "blink.cmp.sources.path",
-            score_offset = 3,
-            fallbacks = { "snippets", "buffer", "ripgrep" },
-            opts = {
-              trailing_slash = false,
-              label_trailing_slash = true,
-              get_cwd = function(context)
-                return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-              end,
-              show_hidden_files_by_default = true,
-            },
-          },
-
-          buffer = {
-            name = "Buffer",
-            enabled = true,
-            max_items = 3,
-            module = "blink.cmp.sources.buffer",
-            min_keyword_length = 3,
-          },
-
-          snippets = {
-            name = "snippets",
-            enabled = true,
-            max_items = 3,
-            module = "blink.cmp.sources.snippets",
-            min_keyword_length = 4,
-            score_offset = 85,
-          },
-
-          ripgrep = {
-            module = 'blink-ripgrep',
-            name = 'Ripgrep',
-            score_offset = -100,
-            opts = {
-              prefix_min_len = 3,
-              context_size = 5,
-              max_filesize = '1M',
-              additional_rg_options = {},
-              search_casing = '--smart-case',
-              fallback_to_regex_highlighting = true,
-            },
-          },
-        },
+        providers = completion_providers,
       },
 
       cmdline = {
@@ -182,16 +201,7 @@ return {
           ['<CR>'] = { 'accept_and_enter', 'fallback' },
         },
 
-        sources = function()
-          local _, type = pcall(vim.fn.getcmdtype)
-          if type == '/' or type == '?' then
-            return { 'buffer', 'path' }
-          end
-          if type == ':' then
-            return { 'cmdline' }
-          end
-          return {}
-        end,
+        sources = cmdline_sources_function,
 
         completion = {
           trigger = {
