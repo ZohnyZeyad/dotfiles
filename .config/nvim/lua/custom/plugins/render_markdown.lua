b58c7739debfcae2bb89fc00450180a7472b93f6
ZohnyZeyad <zeyadalaaeldin20@gmail.com>
2025-03-07 17:34:38 +0100

    Customized render_markdown.

--- .config/nvim/lua/custom/plugins/render_markdown.lua
+++ .config/nvim/lua/custom/plugins/render_markdown.lua
@@ -2,11 +2,45 @@ return {
   'MeanderingProgrammer/render-markdown.nvim',
   lazy = false,
   enabled = true,
-  ft = { "Avante", "codecompanion", "markdown" },
+  ft = { "codecompanion", "markdown" },
   opts = {
     preview = {
-      filetypes = { "Avante", "codecompanion", "markdown" },
+      filetypes = { "codecompanion", "markdown" },
       ignore_buftypes = {},
     },
   },
+
+  config = function()
+    require('render-markdown').setup({
+      code = {
+        enabled = true,
+        render_modes = false,
+        sign = false,
+        style = 'full',
+        position = 'left',
+        language_pad = 0,
+        language_name = true,
+        disable_background = { 'diff' },
+        width = 'block',
+        left_margin = 0,
+        left_pad = 0,
+        right_pad = 1,
+        min_width = 0,
+        border = 'thin',
+        above = '▄',
+        below = '▀',
+        highlight = 'RenderMarkdownCode',
+        highlight_language = nil,
+        inline_pad = 0,
+        highlight_inline = 'RenderMarkdownCodeInline',
+      },
+      heading = {
+        sign = false,
+        -- icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    })
+  end,
 }
