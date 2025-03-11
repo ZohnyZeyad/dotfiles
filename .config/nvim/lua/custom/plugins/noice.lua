8bfdc3e17d1cb1bf8f13d6a42a6b916450c9d22f
ZohnyZeyad <zeyadalaaeldin20@gmail.com>
2025-03-11 17:58:52 +0100

    Updated snacks.nvim keymappings.

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -8,12 +8,7 @@ return {
       view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
       opts = {},              -- global options for the cmdline. See section on views
       format = {
-        -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-        -- view: (default is cmdline view)
-        -- opts: any options passed to the view
-        -- icon_hl_group: optional hl_group for the icon
-        -- title: set to anything or empty string to hide
-        cmdline = { pattern = "^:", icon = "", lang = "vim" },
+        cmdline = { pattern = "^:", icon = ">_", lang = "vim" },
         search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
         search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
         filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
