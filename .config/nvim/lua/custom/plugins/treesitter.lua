612ff988801588be8fdd641c863ab9d65bd2e951
Zohny, Zeyad [PS] <zeyad.zohny@cengage.com>
2025-04-04 13:28:45 +0200

    Disabled broken csv treesitter syntax highlighting.

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -70,7 +70,10 @@ return {
         enable = true,
         use_languagetree = true,
 
-        disable = function(_, buf)
+        disable = function(lang, buf)
+          if lang == "csv" then
+            return true
+          end
           local max_filesize = 100 * 1024 -- 100 KB
           local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
