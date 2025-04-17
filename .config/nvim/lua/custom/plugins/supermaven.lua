4ac7533be48942efc925fd7dc0cddaa0044e038a
Zohny, Zeyad [PS] <zeyad.zohny@cengage.com>
2025-04-17 11:39:27 +0200

    Added Grade and Supermaven, Disabled custom menu.

---
+++ .config/nvim/lua/custom/plugins/supermaven.lua
@@ -0,0 +1,28 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  lazy = false,
+  enabled = true,
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+      },
+      ignore_filetypes = {
+        "markdown", "oil", "AvanteInput", "codecompanion"
+      },
+      color = {
+        -- suggestion_color = "#ffffff",
+        cterm = 244,
+      },
+      log_level = "off",                 -- set to "off" to disable logging completely
+      disable_inline_completion = false, -- disables inline completion for use with cmp
+      disable_keymaps = false,           -- disables built in keymaps for more manual control
+      condition = function()
+        return false
+      end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+    })
+  end,
+  keys = { { "<Leader>smt", "<cmd>SuperMavenToggle<cr>", desc = "[S]uper [M]aven [T]oggle" } },
+}
