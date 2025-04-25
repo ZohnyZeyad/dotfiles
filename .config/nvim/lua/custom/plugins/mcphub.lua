2f3c2b66c3fc5a95f98167c067ce81e4739e37f4
Zohny, Zeyad [PS] <zeyad.zohny@cengage.com>
2025-04-25 17:55:55 +0200

    Disabled MCPHub lazy loading.

--- .config/nvim/lua/custom/plugins/mcphub.lua
+++ .config/nvim/lua/custom/plugins/mcphub.lua
@@ -4,7 +4,7 @@ return {
     "nvim-lua/plenary.nvim",
   },
   -- comment the following line to ensure hub will be ready at the earliest
-  cmd = "MCPHub", -- lazy load by default
+  -- cmd = "MCPHub", -- lazy load by default
   build = "npm install -g mcp-hub@latest",
   -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
   -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
