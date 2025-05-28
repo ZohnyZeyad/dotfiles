531bbef4d778e26cc5dcb22b3144c20d5c691cba
Zohny, Zeyad [PS] <zeyad.zohny@cengage.com>
2025-05-28 16:41:23 +0200

    Updated Gemini models.

--- .config/nvim/lua/custom/plugins/codecompanion.lua
+++ .config/nvim/lua/custom/plugins/codecompanion.lua
@@ -1,7 +1,7 @@
 local gemini_models = { ---@see https://ai.google.dev/gemini-api/docs/models/gemini
   "gemini-2.0-flash",
-  "gemini-2.5-flash-preview-04-17",
-  "gemini-2.5-pro-exp-03-25",
+  "gemini-2.5-flash-preview-05-20",
+  "gemini-2.5-pro-preview-05-06",
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-1.5-pro",
 }
